85fe08093d49583d7d3cd270830398e2a687f8ad
Sreekar Reddy Velayudham <60206516+Sreekar13@users.noreply.github.com>
2021-04-18 18:25:51 -0400

    Upload image to docker hub using upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,7 +5,7 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=sv739/udacity-model:1.0
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +13,5 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+sudo docker tag model:1.0 sv739/udacity-model:1.0
+sudo docker push sv739/udacity-model:1.0
