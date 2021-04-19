58c4f630c53b8f95f5bf428f6900060983f132c9
Sreekar Reddy Velayudham <60206516+Sreekar13@users.noreply.github.com>
2021-04-19 17:20:05 -0400

    kubernetes realted commits

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sv739/udacity-model:1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run modeldemo \
+      --generator=run-pod/v1\
+      --image=$dockerpath\
+      --port=80 --labels app=model
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+sleep 30
+kubectl port-forward modeldemo 8000:80
